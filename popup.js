368aa600a93c01ab2762cde26fcfa4ad63d5c380
Yanez <sergioy@amazon.com>
2017-06-24 15:23:23 -0700

    layout changes

--- popup.js
+++ popup.js
@@ -9,6 +9,22 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
       news_element.find("#article-title").first().html(page_info["title"]);
       news_element.find("#article-img").first().css({"background-image" : "url('"+page_info["image"]+"')"});
       news_element.find("#article-description").first().html(page_info["description"]);
+      const items = ["caution", "unreliable", "trustable"];
+      const descriptions = {
+        "caution": "we advise you to read carefully and not to trust blindly this article.",
+        "unreliable": "we advise against reading and sharing this article, it's been published in an unreliable source.",
+        "trustable": "we have enough reasons to believe this article is reliable."
+      }
+      const icons = {
+        "caution": "fa-exclamation-triangle",
+        "unreliable": "fa-hand-paper-o",
+        "trustable": "fa-shield"
+      }
+      var item = items[Math.floor(Math.random()*items.length)];
+      news_element.find('#explanation-text').first().html(descriptions[item]);
+      news_element.find("#article-verdict").first().addClass(item);
+      news_element.find("#verdict-icon").first().addClass(icons[item]);
+      news_element.find("#verdict-title").first().html(item.toUpperCase());
       news_element.show();
       unavailable_element.hide();
     } else {  
