a7751c81ca4119522c18acc36755e69c964704f1
Sergio Yáñez <sergioyanez@Sergios-MacBook-Pro.local>
2017-06-24 21:34:02 -0700

    General commit for devpost

--- popup.js
+++ popup.js
@@ -7,9 +7,13 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
       page_info = request.source;
       //news_element.html("here "+JSON.stringify(request.source, null, 2));
       news_element.find("#article-title").first().html(page_info["title"]);
+      news_element.find(".form-title").val(page_info["title"]);
       news_element.find("#article-img").first().css({"background-image" : "url('"+page_info["image"]+"')"});
+      news_element.find(".form-image").val(page_info["image"]);
       news_element.find("#article-description").first().html(page_info["description"]);
-      const items = ["caution", "unreliable", "trustable"];
+      news_element.find(".form-description").val(page_info["description"]);
+      news_element.find(".form-url").val(page_info["url"]);
+      const items = ["unreliable", "caution", "trustable"];
       const descriptions = {
         "caution": "we advise you to read carefully and not to trust blindly this article.",
         "unreliable": "we advise against reading and sharing this article, it's been published in an unreliable source.",
@@ -20,13 +24,79 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
         "unreliable": "fa-hand-paper-o",
         "trustable": "fa-shield"
       }
-      var item = items[Math.floor(Math.random()*items.length)];
-      news_element.find('#explanation-text').first().html(descriptions[item]);
-      news_element.find("#article-verdict").first().addClass(item);
-      news_element.find("#verdict-icon").first().addClass(icons[item]);
-      news_element.find("#verdict-title").first().html(item.toUpperCase());
+      
       news_element.show();
       unavailable_element.hide();
+      $.ajax({
+        method: "GET",
+        url: "https://newbie-c0810.firebaseio.com/articles.json?orderBy=%22url%22&startAt=%22"+page_info["url"]+"%22&endAt=%22"+page_info["url"]+"%22"
+      })
+        .done(function( msg ) {
+          if (Object.keys(msg).length == 0 || !msg[Object.keys(msg)[0]].ratings){
+            $("#unknown-form").show();
+            $("#load").html("There's no information for this article yet.");
+          } else {
+            var name = Object.keys(msg)[0];
+            var newstar = '<i class="fa fa-star" aria-hidden="true"></i>';
+            var newstarvoid = '<i class="fa fa-star-o" aria-hidden="true"></i>';
+            var ratings = msg[name].ratings;
+            var total = 0.0;
+            for (var i = 0; i < Object.keys(ratings).length; i++){
+              var iname = Object.keys(ratings)[i];
+              var rate = parseInt(ratings[iname].rating);
+              total += rate;
+            }
+            var score = total / Object.keys(ratings).length;
+            console.log(msg[name]);
+            var newbie_score = parseFloat(msg[name].newbie_rating)
+            var readersStarsHTML = "";
+            for (var i = 1; i <= score; i++){
+              readersStarsHTML += newstar;
+            }
+            for (var i = Math.ceil(score); i < 5; i++){
+              readersStarsHTML += newstarvoid;
+            }
+            console.log(readersStarsHTML);
+            if (msg[name].source_num_ratings != 0){
+              var source_score = parseFloat(msg[name].source_rep_total) / parseFloat(msg[name].source_num_ratings)
+            } else {
+              var source_score = score;
+            }
+            var sourceStarsHTML = "";
+            for (var i = 1; i <= source_score; i++){
+              sourceStarsHTML += newstar;
+            }
+            for (var i = Math.ceil(source_score); i < 5; i++){
+              sourceStarsHTML += newstarvoid;
+            }
+            var newbieStarsHTML = "";
+            for (var i = 1; i <= newbie_score; i++){
+              newbieStarsHTML += newstar;
+            }
+            for (var i = Math.ceil(newbie_score); i < 5; i++){
+              newbieStarsHTML += newstarvoid;
+            }
+            console.log("NB");
+            console.log(newbie_score);
+            $("#eval-stars").html(readersStarsHTML);
+            $("#source-stars").html(sourceStarsHTML);
+            $("#newbie-stars").html(newbieStarsHTML);
+            $("#eval-text").html(Math.floor(score*100)/100.0);
+            $("#source-text").html(Math.floor(source_score*100)/100.0);
+            $("#newbie-text").html(Math.floor(newbie_score*100)/100.0);
+            var overall_score = (score + newbie_score + source_score) / 3.0;
+            if (overall_score < 2) var item = items[0];
+            else if (overall_score < 4.5) var item = items[1];
+            else item = items[2];
+            news_element.find('#explanation-text').first().html(descriptions[item]);
+            news_element.find("#article-verdict").first().addClass(item);
+            news_element.find("#verdict-icon").first().addClass(icons[item]);
+            news_element.find("#verdict-title").first().html(item.toUpperCase());
+            $("#article-loading").hide();
+            $("#article-summary").show();
+          }
+        });
+      page_info = null;
     } else {  
       news_element.hide();
       unavailable_element.show();
